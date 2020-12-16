566299593bbf81360171f0837b444b5628956ee6
ESTEFANIA ARISBETH RAMIREZ GONZALEZ <I20050032@monclova.tecnm.mx>
2020-12-16 12:27:48 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoenConsola/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProyectoenConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora en resultado binario");
+            Console.WriteLine("Dar diseño de la calculadora");
+            Console.WriteLine("Craer el codigo para que de el resultado binario");
+            Console.WriteLine("Proyecto final de consola");
+        }
+    }
+}
